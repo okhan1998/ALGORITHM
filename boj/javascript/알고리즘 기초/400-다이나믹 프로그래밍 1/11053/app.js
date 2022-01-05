f31a3311d852a138bb5efdc1a816e73c1efefb29
okhan1998 <whwlgns111@gmail.com>
2022-01-05 15:36:11 +0900

    solve 11053

---
+++ boj/javascript/알고리즘 기초/400-다이나믹 프로그래밍 1/11053/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let numbers = fs.readFileSync(filepath).toString().split("\n");
+const N = Number(numbers[0]);
+numbers.shift();
+numbers = numbers[0].split(" ").map((element) => Number(element));
+
+const dp = new Array(N).fill(1);
+for (let i = 1; i < N; i++) {
+  const values = [1];
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] > numbers[j]) {
+      values.push(dp[j] + 1);
+    }
+  }
+  dp[i] = Math.max(...values);
+}
+console.log(Math.max(...dp));
