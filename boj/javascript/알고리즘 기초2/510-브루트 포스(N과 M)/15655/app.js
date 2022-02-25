670f01a2ddd06fdcc9abbb11cdb460c456b17457
okhan1998 <whwlgns111@gmail.com>
2022-02-25 21:38:32 +0900

    solve 15655

---
+++ boj/javascript/알고리즘 기초2/510-브루트 포스(N과 M)/15655/app.js
@@ -0,0 +1,30 @@
+let fs = require('fs')
+let filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt' 
+let input = fs.readFileSync(filepath).toString().split('\n');
+let [N, M] = input[0].split(' ').map(x => +x);
+let arr = input[1].split(' ').map(x => +x);
+arr.sort((a,b) => a-b)
+let list = [];
+let ret = [];
+
+let dfs = (cnt, M, N) => {
+    if(cnt == M){
+        ret.push(list.join(' '))
+        return ;
+    }
+    for(let i = 0; i < N; i++){
+        if(cnt == 0){
+            list[cnt] = arr[i]
+            dfs(cnt+1, M, N)
+        } else {
+            list[cnt] = arr[i]
+            if(list[cnt] > list[cnt-1])
+                dfs(cnt+1, M, N)
+        }
+
+    }
+    return ;
+}
+
+dfs(0, M, N)
+console.log(ret.join('\n'));
