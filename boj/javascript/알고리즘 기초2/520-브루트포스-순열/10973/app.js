9862803ff1b5ed13211b6472f6ed8a50165434e5
okhan1998 <whwlgns111@gmail.com>
2022-03-07 16:57:13 +0900

    solve 10973

---
+++ boj/javascript/알고리즘 기초2/520-브루트포스-순열/10973/app.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+let input = fs.readFileSync(filepath).toString().split('\n');
+let N = +input[0];
+let arr = input[1].split(' ').map(x => +x);
+let arrCopy = JSON.parse(JSON.stringify(arr));
+arrCopy.sort((a,b) => a-b);
+
+const previousPermutation = (N, arr) => {
+    if(arr.every((v,i) => v === arrCopy[i])){
+        console.log(-1);
+        return;
+    } else {
+        let i = N - 2;
+        while(arr[i] < arr[i+1]) {
+            i--;
+        }
+        let j = N - 1;
+        while(arr[i] < arr[j]){
+            j--;
+        }
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        console.log([...arr.slice(0,i+1), ...arr.slice(i+1).sort((a,b) => b-a)].join(' ').trim()); 
+        return;
+    }
+}
+previousPermutation(N,arr)
