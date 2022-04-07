8910b44b284073e7584c4fd74bbb30a01daeb944
okhan1998 <whwlgns111@gmail.com>
2022-04-07 16:13:55 +0900

    solve 2178

--- boj/javascript/알고리즘 기초2/600-그래프1/2178/appbfs.js
+++ boj/javascript/알고리즘 기초2/600-그래프1/2178/appbfs.js
@@ -6,8 +6,7 @@ input = input.map(x => x.split('').map(y => +y));
 let check1 = new Array(H*W+1).fill(false);
 let check2 = [];
 let adjArr = Array.from(Array(H*W+1), () => Array(0))
-let min = 1000000000;
-let list = [];
+let bfsarr = []
 for(let i=0; i<H; i++){// 방문가능 여부 1차원 배열로 만들어주기 
     for(let j =0; j<W; j++){
         check2.push(input[i][j])
@@ -30,31 +29,23 @@ for(let i=1; i<= H*W; i++){ // 인접리스트 만들기
 }
 check2.unshift(0);
 
-// console.log(check1)
-// console.log(check2)
-// console.log(adjArr);
-
-let dfs = (cnt, V) => {
-    if(check1[H*W]){
-        if(min > list.length)
-            min = list.length;
-        // dfsarr.push(JSON.parse(JSON.stringify(list)))
-        return ; 
-    }
-    for(let i =0; i<adjArr[V].length; i++){ //조합문제 dfs로 풀듯이 풀었음 단 조합에서는 cnt가 원하는 갯수가 되었을때 종료했는데 여기서는 목적지에 도착하면 종료하듯이 만들었음 
-        if(!check1[V] && check2[V]){
-        check1[V] = true;
-        list[cnt] = V;
-        let next = adjArr[V][i]
-        if(!check1[next] && check2[next])
-            dfs(cnt+1, next);
-        check1[V] = false;
-        list.pop()
+let bfs = (V) => {
+    let queue = [];
+    queue.push(V)
+    check1[V] = true;
+    while(queue.length != 0){
+        
+        let now = queue.shift()
+        bfsarr.push(now)
+        for(let i=0; i<adjArr[now].length; i++){
+            if(!check1[adjArr[now][i]] && check2[adjArr[now][i]]){
+                check1[adjArr[now][i]] = true;
+                queue.push(adjArr[now][i])
+            }
         }
     }
-    
+    return ;
 }
+bfs(1);
+console.log(bfsarr)
 
-dfs(0, 1)
-console.log(min)
-// console.log(dfsarr);
---
+++ boj/javascript/알고리즘 기초2/600-그래프1/2178/내생각에모범답안.js
@@ -0,0 +1,30 @@
+const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+const [yMax, xMax] = input.shift().split(" ");
+const map = input.map(v => v.split("").map(x => +x));
+
+const stack = [[0, 0, 0]];
+
+const dir = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0]
+];
+
+while (stack.length) {
+  const [x, y, dis] = stack.shift();
+
+  for (let i = 0; i < 4; i++) {
+    const xPos = x + dir[i][0];
+    const yPos = y + dir[i][1];
+        
+    if (0 <= xPos && 0 <= yPos && xPos < xMax && yPos < yMax) {
+      if (map[yPos][xPos] === 1) {
+        map[yPos][xPos] = dis
+        stack.push([xPos, yPos, dis+1]);
+      }
+    }
+  }
+}
+console.log(map);
+console.log(map[yMax-1][xMax-1]);
